bcbb5b241230d2cc16331a9f2ee041c319500c17
kristjanjansen <kristjanjansen@users.noreply.github.com>
2020-05-11 14:46:01 +0000

    Build the library

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Test_1 = require("./Test");
+var Test_1 = require("./components/Test");
 exports.Test = Test_1.Test;
 var message = "test_branch..";
 exports.message = message;
